6e21b10f74065d01e9285fab14bbf65bdeea13b9
WIDU <wdulek@gmail.com>
2020-09-30 19:54:01 +0200

    v3

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
-const {app, BrowserWindow } = require('electron');
+const {app, BrowserWindow, ipcMain, dialog } = require('electron');
 const fs = require('fs');
 
 let mainWindow = null;
 
 app.on('ready', () => {
-    let mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         x: "200px", 
         y: "150ppx",
         show: false,
@@ -17,16 +17,89 @@ app.on('ready', () => {
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
-        file = "/Volumes/Mac SSD/Users/annaraurowicz/graphviz-example/graph3.dot";
-        const content = fs.readFileSync(file).toString();
-        console.log(content);
-        mainWindow.webContents.send('file-opened', file, content);
 
+        ipcMain.on('run-command', (event, c, a1, a2, a3, dot) => {
+            console.log(c, a1, a2, a3) // prints "ping"
+        
+            if (a1 != null) {
+        
+            var exec = require('child_process').exec
+            function Callback(err, stdout, stderr) {
+                if (err) {
+                    console.log(`exec error: ${err}`);
+                    console.log('error koniec');
+                    return;
+                    }else{
+                    console.log(`${stdout}`);
+                    // file = "/Users/wdulek001/Documents/ProcessLibrary/FaktsPresentation/res_graph1.dot";
+                    file = dot;
+                    const content = fs.readFileSync(file).toString();
+                    console.log(content);
+                    event.reply('file-opened', file, content);
+                    console.log('success koniec');
+                }
+            }
+        
+            const cmd = c + ' ' + a1 + ' ' + a2 + ' "' + a3.replace(/\n/g, " "); + '"';
+            console.log(cmd);
+                // res = exec('cd /Users/wdulek001/Documents/ProcessLibrary/FaktsPresentation & rdf_process faktspresentation.process', Callback);
+                // res = exec('rdf_process /Users/wdulek001/Documents/ProcessLibrary/FaktsPresentation/faktspresentation.process', Callback);
+                res = exec(cmd, Callback);
+            // event.reply('asynchronous-reply', 'pong')
+            } else {
+                file = c;
+                const content = fs.readFileSync(file).toString();
+                console.log(content);
+                event.reply('file-opened', file, content);
+                console.log('success koniec');
+            }
+        
+          });
+        
+
+        ipcMain.on('get-file-message', (event, arg) => {
+            console.log(arg) // prints "ping"
+            // const files = dialog.showOpenDialogSync(mainWindow, {
+            //     properties: ['openFile'],
+            //     filters: [
+            //         {name: 'Dot file', extensions: ['dot']}
+            //     ]     
+            // });
+            event.returnValue = 'pong';
+          }
+        );
+
+        
+        // mainWindow.removeMenu();
+
+        // ************************************8
+        // var exec = require('child_process').exec
+        // function Callback(err, stdout, stderr) {
+        // if (err) {
+        //     console.log(`exec error: ${err}`);
+        //     console.log('error koniec');
+        //     return;
+        //     }else{
+        //     console.log(`${stdout}`);
+        //     file = "/Users/wdulek001/Documents/ProcessLibrary/FaktsPresentation/res_graph1.dot";
+        //     const content = fs.readFileSync(file).toString();
+        //     console.log(content);
+        //     mainWindow.webContents.send('file-opened', file, content);
+        //     console.log('success koniec');
+        //     }
+        // }
+
+        // res = exec('cd /Users/wdulek001/Documents/ProcessLibrary/FaktsPresentation & rdf_process faktspresentation.process', Callback);
+        //****************************************************** */
+        
         //getFileFromUser();
         //mainWindow.webContents.openDevTools();
     });
 
+
     mainWindow.on('closed', () => {
         mainWindow = null;
-    })
-})
+    });
+});
+
+
--- app/renderer.js
+++ app/renderer.js
@@ -4,6 +4,15 @@ const {remote, ipcRenderer, shell} = require('electron');
 
 const output = document.getElementById('output');
 const dotF = document.getElementById('dot');
+const open_dot_file = document.getElementById('open-dot-file');
+const button_get_param_1 = document.getElementById('get-param-1');
+const command_name = document.getElementById('command-name');
+const command_param_1 = document.getElementById('command-param-1');
+const command_param_2 = document.getElementById('command-param-2');
+const command_param_3 = document.getElementById('command-param-3');
+const dot_file = document.getElementById('dot-file');
+
+
 var dotFile = '';
 
 ipcRenderer.on('file-opened', (event, file, content) => {
@@ -16,3 +25,29 @@ ipcRenderer.on('file-opened', (event, file, content) => {
     dotF.dispatchEvent(event);
 
 });
+
+const runCommand = document.getElementById('runCommand');
+sendCommand = () => {
+
+  let choice = document.getElementById("Choice2").checked;
+
+  
+
+  if (choice == true) {
+    ipcRenderer.send('run-command', dot_file.value);
+  } else {
+    ipcRenderer.send('run-command', command_name.value , command_param_1.value, command_param_2.value, command_param_3.value, dot_file.value);
+  }
+  
+};
+runCommand.addEventListener("click", sendCommand);
+
+// pobieranie nazwy pliku
+
+const getFileName = (field1) => {
+  field1.value = ipcRenderer.sendSync('get-file-message', 'ping');
+};
+
+
+button_get_param_1.addEventListener('click', getFileName(command_param_1));
+open_dot_file.addEventListener('click', getFileName(command_param_1));
